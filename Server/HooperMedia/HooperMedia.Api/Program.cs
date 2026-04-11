57a3a5fef895606160f73040e1b905c8e27efb05
Gopi <gopi.govind@olivejar.co.uk>
2026-04-11 16:24:03 +0100

    Docker compose creation

--- Server/HooperMedia/HooperMedia.Api/Program.cs
+++ Server/HooperMedia/HooperMedia.Api/Program.cs
@@ -1,7 +1,9 @@
+using HooperMedia.Infrastructure.Data;
 using HooperMedia.Infrastructure.Repositories;
 using HooperMedia.Infrastructure.Repositories.Interfaces;
 using HooperMedia.Infrastructure.Services;
 using HooperMedia.Infrastructure.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +12,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
 builder.Services.AddScoped<IPersonService, PersonService>();
 
-
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
